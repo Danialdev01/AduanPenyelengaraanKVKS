c6edd0151b9bc81563d23f9aaaea3c4ea2151b6d
Danial Irfan <developersdani@gmail.com>
2023-11-28 18:14:29 +0800

    test bug from prod

--- index.php
+++ index.php
@@ -43,6 +43,17 @@
                         data-te-ripple-color="light">
                         Sign in
                     </button>
+
+                    <select data-te-select-init>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                        <option value="4">Four</option>
+                        <option value="5">Five</option>
+                        <option value="6">Six</option>
+                        <option value="7">Seven</option>
+                        <option value="8">Eight</option>
+                    </select>
     
                 </form>
             </div>
