cbd43a26d3e8ecf291c1377a3547506733bbb01f
Danial Irfan <developersdani@gmail.com>
2023-11-28 18:47:02 +0800

    idk i hate tailwind ...

--- components/footer.php
+++ components/footer.php
@@ -1,2 +1,2 @@
-<script src="<?php echo $location_index?>/dist/tw-elements.umd.min.js"></script>
+<!-- <script src="<?php echo $location_index?>/dist/tw-elements.umd.min.js"></script> -->
 <!-- <script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/tw-elements.umd.min.js"></script> -->
