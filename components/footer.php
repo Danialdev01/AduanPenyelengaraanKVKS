41f15ebbb4f0a3f0d81869f87a573e09db612edf
aisardin <nopinio70@gmail.com>
2026-01-19 14:37:14 +0800

    updated footer

--- components/footer.php
+++ components/footer.php
@@ -4,41 +4,44 @@
 <footer
   class="flex flex-col items-center bg-neutral-100 text-center dark:bg-neutral-600 lg:text-left">
   <div class="container p-6 text-neutral-800 dark:text-neutral-200">
-    <div class="grid gap-4 lg:grid-cols-2">
+    <div>
       <div class="mb-6 md:mb-0">
 
-        <p class="mb-0">
+        <p class="mb-0" style="text-align:center;margin-bottom:10px;">
           Sistem e-Penyelenggaraan memudahkan warga KV Kuala Selangor membuat aduan penyelenggaraan secara dalam talian. Sistem ini dibangunkan oleh pelajar Jabatan Teknologi Maklumat dan digunakan oleh Unit Penyelenggaraan KVKS bagi pengurusan aduan yang lebih cekap dan sistematik.
         </p>
       </div>
 
-      <div class="mb-6 md:mb-0">
+      <div class="mb-6 md:mb-0" style="text-align:center;">
 
-        <p class="mb-0">
-          <span>
+        <p class="mb-0" style="text-align: center;">
+  <span>
+    Kolej Vokasional Kuala Selangor, 45600 Bestari Jaya, Selangor<br>
+    <span style="display: flex; justify-content: center; align-items: center; flex-wrap: wrap;">
+      <svg style="color:black;padding-right: 5px;" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+          stroke-width="2" d="M11 16v-5.5A3.5 3.5 0 0 0 7.5 7m3.5 9H4v-5.5A3.5 3.5 0 0 1 7.5 7m3.5 9v4M7.5 7H14m0 0V4h2.5M14 7v3m-3.5 6H20v-6a3 3 0 0 0-3-3m-2 9v4m-8-6.5h1"/>
+      </svg>
+      <!-- Mengubah margin-left dari 8px menjadi 0 -->
+      <span style="margin-left: 0;">BHA3001@moe.edu.my</span>
+    </span>
+</span>
 
-            Kolej Vokasional Kuala Selangor, 45600 Bestari Jaya, Selangor<br>
-            <span style="display: flex;">
-
-              <svg style="color:black;padding-right: 5px;" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 16v-5.5A3.5 3.5 0 0 0 7.5 7m3.5 9H4v-5.5A3.5 3.5 0 0 1 7.5 7m3.5 9v4M7.5 7H14m0 0V4h2.5M14 7v3m-3.5 6H20v-6a3 3 0 0 0-3-3m-2 9v4m-8-6.5h1"/>
-              </svg>
-
-              BHA3001@moe.edu.my<br>
-            </span>
-            <span class="pt-2 flex text-center lg:justify-start justify-center">
-              <span style="padding-right: 5px;">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
-                stroke="currentColor" class="relative  mt-1 h-4 w-4 text-black">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
-                </svg>
-              </span>
-              <span>03-32718370</span>
-            </span>
-            <br>
-          </span>
-        </p>
+<!-- Bagian kedua -->
+<div style="display: flex; justify-content: center; width: 100%;">
+  <span class="pt-2 flex text-center lg:justify-start justify-center" style="display: flex; align-items: center;">
+    <span style="padding-right: 5px;">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+        stroke="currentColor" class="relative mt-1 h-4 w-4 text-black">
+        <path stroke-linecap="round" stroke-linejoin="round"
+          d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+      </svg>
+    </span>
+    <span style="margin: 0;">03-32718370</span>
+  </span>
+</div>
+</div>
+</p>
       </div>
     </div>
   </div>
@@ -49,7 +52,7 @@
     © <?php $year = date("Y"); echo $year; ?> Copyright 
     <a
       class="text-neutral-800 dark:text-neutral-400"
-      href="https://kvkualaselangor.com/"
+      href="https://kvkualaselangor.com/" 
       >Kolej Vokasional Kuala Selangor</a
     >
   </div>
